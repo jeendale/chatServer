986856ff1e35c612bbbaf9ea2230b8465dd6bfd0
jeendale <jeekoo7203@gmail.com>
2024-01-15 02:55:15 +0900

    feat: JwtAuthorizationFilter 인증정보확인에 사용

---
+++ src/main/java/com/spring/chatserver/domain/user/dto/CommonResponseDto.java
@@ -0,0 +1,15 @@
+package com.spring.chatserver.domain.user.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class CommonResponseDto {
+    private String message;
+    private Integer statusCode;
+}
