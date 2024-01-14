c527727f1ff97428bc8f74a8b2f46e443667cc25
jeendale <jeekoo7203@gmail.com>
2024-01-15 03:51:45 +0900

    feat: 매새지 return값 생성

--- src/main/java/com/spring/chatserver/domain/chat/controller/ChatController.java
+++ src/main/java/com/spring/chatserver/domain/chat/controller/ChatController.java
@@ -1,22 +1,32 @@
 package com.spring.chatserver.domain.chat.controller;
 
 import com.spring.chatserver.domain.chat.dto.request.ChatReq;
+import com.spring.chatserver.domain.chat.dto.response.ChatRes;
+import com.spring.chatserver.domain.chat.dto.response.MessageRes;
+import com.spring.chatserver.domain.chat.dto.response.MessageResList;
 import com.spring.chatserver.domain.chat.entity.PublishMessage;
 import com.spring.chatserver.domain.chat.service.ChatService;
+import com.spring.chatserver.domain.user.security.UserDetailsImpl;
+import com.spring.chatserver.global.exception.GlobalException;
+import com.spring.chatserver.global.meta.ResultCode;
+import com.spring.chatserver.global.response.RestResponse;
+import com.spring.chatserver.global.validate.UserValidator;
 import jakarta.annotation.Resource;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class ChatController {
     //private SimpMessagingTemplate template;
     private ChatService chatService;
@@ -28,7 +38,8 @@ public class ChatController {
 
     //pub/chats/meessage/{room-id}으로 맵핑
     @MessageMapping("/chats/message/{room-id}")
-    public void message(@DestinationVariable("room-id")Long roomId, ChatReq chatReq){
+    public RestResponse<ChatRes> message(@DestinationVariable("room-id")Long roomId, ChatReq chatReq){
+        ChatRes chatRes=new ChatRes();
         PublishMessage publishMessage
                 = new PublishMessage(chatReq.getRoomId(), chatReq.getSenderId(), chatReq.getContent(), LocalDateTime.now());
 
@@ -36,6 +47,8 @@ public class ChatController {
         redisTemplate.convertAndSend(topic.getTopic(),publishMessage);
         //메세지 캐시처리
         chatService.message(chatReq,roomId);
+
+        return RestResponse.success(chatRes);
     }
 
 
---
+++ src/main/java/com/spring/chatserver/domain/chat/dto/response/ChatRes.java
@@ -0,0 +1,7 @@
+package com.spring.chatserver.domain.chat.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties
+public class ChatRes {
+}
